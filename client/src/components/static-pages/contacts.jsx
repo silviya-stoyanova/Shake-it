7dc84f1b74d5ddff3b5b6c076d79b73643a8cb67
Silviya Stoyanova <silviya-stoyanova@users.noreply.github.com>
2019-11-16 00:17:21 +0200

    removed ContactsMap component

--- client/src/components/static-pages/contacts.jsx
+++ client/src/components/static-pages/contacts.jsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment as div } from 'react'
 import '../../static/css/forms.css'
-//import ContactsMap from '../static-pages/contacts-map'
 
 class ContactForm extends Component {
     constructor(props) {
@@ -19,9 +18,6 @@ class ContactForm extends Component {
 
         return (
             <div>
-                {/* <ContactsMap google={google} /> */}
-                {/* </div><iframe src="https://www.google.com/maps/place/%D0%AF%D0%BC%D0%B1%D0%BE%D0%BB/@42.4771342,26.4679302,13z/data=!3m1!4b1!4m5!3m4!1s0x40a6342956b710b5:0xf4177c0a63985b6a!8m2!3d42.4841999!4d26.5035023"> */}
-                {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5842.18144046166!2d26.508194563092392!3d42.48606336154913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a634309f91e3c9%3A0x51b0a9e04be4122f!2z0J7QsdGJ0LjQvdCwINCv0LzQsdC-0Ls!5e0!3m2!1sbg!2sbg!4v1573846071952!5m2!1sbg!2sbg"></iframe> */}
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5842.18144046166!2d26.508194563092392!3d42.48606336154913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a634309f91e3c9%3A0x51b0a9e04be4122f!2z0J7QsdGJ0LjQvdCwINCv0LzQsdC-0Ls!5e0!3m2!1sbg!2sbg!4v1573846071952!5m2!1sbg!2sbg" style={{ width: "600", height: "450", frameborder: "0", style: "border:0", allowfullscreen: "" }}></iframe>
 
                 <div className="form">
