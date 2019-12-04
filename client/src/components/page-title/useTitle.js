da21431834b971abace919c96d901caa6f23980d
Silviya Stoyanova <silviya-stoyanova@users.noreply.github.com>
2019-12-04 22:35:17 +0200

    update my custom react hook - useTitle

--- client/src/components/page-title/useTitle.js
+++ client/src/components/page-title/useTitle.js
@@ -4,12 +4,13 @@ import { useEffect } from 'react'
 // It allows combining Hooks provided by React into your own abstractions,
 // and reuse common stateful logic between different components.
 const useTitle = (title) => {
-    // since useEffect hook can be used only within a funtional component
-    // and is an alternative for componentDidMount, componentDidUpdate, and componentWillUnmount,
-    // adding an empty array as a second argument will act like componentDidMount
+    // useEffect hook can be used only within a funtional component
+    // it is an alternative for componentDidMount, componentDidUpdate, and componentWillUnmount,
     useEffect(() => {
         document.title = `Shake it - ${title}`
-    }, [])
+        // adding [title] as a second argument will cause a component re-render
+        //only when <title> is changed
+    }, [title])
 }
 
 export default useTitle
